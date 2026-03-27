b020d79dbc26fe801b2ca6ec83ffbd6b08acfdba
michaela <@email.com>
2026-03-27 15:05:43 +0200

    Chains

---
+++ Assets/Props/CombinationPadLock/Script/PadLockEmissionColor.cs
@@ -0,0 +1,40 @@
+﻿// Script by Marcelli Michele
+
+using UnityEngine;
+
+public class PadLockEmissionColor : MonoBehaviour
+{
+    TimeBlinking tb;
+
+    private GameObject _myRuller;
+
+    [HideInInspector]
+    public bool _isSelect;
+
+    [SerializeField] public Color blinkingColor = Color.yellow;
+
+    private void Awake()
+    {
+        tb = FindObjectOfType<TimeBlinking>();
+    }
+    void Start()
+    {
+        _myRuller = gameObject;
+    }
+
+
+    public void BlinkingMaterial()
+    {
+        _myRuller.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+
+        if (_isSelect)
+        {
+            _myRuller.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.Lerp(Color.clear, blinkingColor, Mathf.PingPong(Time.time, tb.blinkingTime)));
+        }
+        if (_isSelect == false)
+        {
+            _myRuller.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.clear);
+        }
+
+    }
+}
---
+++ Assets/Props/CombinationPadLock/Script/PadLockPassword.cs
@@ -0,0 +1,36 @@
+﻿// Script by Marcelli Michele
+
+using System.Linq;
+using UnityEngine;
+
+public class PadLockPassword : MonoBehaviour
+{
+    MoveRuller _moveRull;
+
+    public bool isPasswordCorrect = false;
+
+    public int[] _numberPassword = { 0, 0, 0, 0 };
+
+    private void Awake()
+    {
+        _moveRull = FindObjectOfType<MoveRuller>();
+    }
+
+    public void Password()
+    {
+        if (_moveRull._numberArray.SequenceEqual(_numberPassword))
+        {
+            // Here enter the event for the correct combination
+            Debug.Log("Password correct");
+            isPasswordCorrect = true;
+
+            // Es. Below the for loop to disable Blinking Material after the correct password
+            for (int i = 0; i < _moveRull._rullers.Count; i++)
+            {
+                _moveRull._rullers[i].GetComponent<PadLockEmissionColor>()._isSelect = false;
+                _moveRull._rullers[i].GetComponent<PadLockEmissionColor>().BlinkingMaterial();
+            }
+
+        }
+    }
+}
